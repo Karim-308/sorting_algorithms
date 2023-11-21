48109663b2138fcbbd019d750cbf78337493b2e3
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-21 03:43:40 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -27,6 +27,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void merge(int *array, int *tmp, int start, int mid, int end);
 void merge_sort_bottom_up(int *array, size_t size);
 void merge_sort_recursive(int *array, int *tmp, int start, int end);
 void merge(int *left, size_t left_size, int *right, size_t right_size);
