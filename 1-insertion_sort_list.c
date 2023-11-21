a8fd87bbdf53858c68b3f016f8b7d432de4de897
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-21 03:05:14 +0200

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,20 +8,20 @@
  */
 void swap_nodes(listint_t **list, listint_t *node)
 {
-    if (node->prev != NULL && node->n < node->prev->n)
-    {
-        /* Swap the nodes */
-        node->prev->next = node->next;
-        if (node->next != NULL)
-            node->next->prev = node->prev;
-        node->next = node->prev;
-        node->prev = node->prev->prev;
-        node->next->prev = node;
+	if (node->prev != NULL && node->n < node->prev->n)
+	{
+		/* Swap the nodes */
+		node->prev->next = node->next;
+		if (node->next != NULL)
+			node->next->prev = node->prev;
+		node->next = node->prev;
+		node->prev = node->prev->prev;
+		node->next->prev = node;
 
-        /* Update the list pointer if needed */
-        if (node->prev == NULL)
-            *list = node;
-    }
+		/* Update the list pointer if needed */
+		if (node->prev == NULL)
+			*list = node;
+	}
 }
 
 /**
@@ -32,23 +32,23 @@ void swap_nodes(listint_t **list, listint_t *node)
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current_node;
-    listint_t *temp_node;
+	listint_t *current_node;
+	listint_t *temp_node;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    current_node = (*list)->next;
+	current_node = (*list)->next;
 
-    while (current_node != NULL)
-    {
-        temp_node = current_node->next;
+	while (current_node != NULL)
+	{
+		temp_node = current_node->next;
 
-        while (current_node->prev != NULL && current_node->n < current_node->prev->n)
-        {
-            swap_nodes(list, current_node);
-        }
+		while (current_node->prev != NULL && current_node->n < current_node->prev->n)
+		{
+			swap_nodes(list, current_node);
+		}
 
-        current_node = temp_node;
-    }
+		current_node = temp_node;
+	}
 }
