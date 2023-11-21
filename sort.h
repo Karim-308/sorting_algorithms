d1dbb36858214170c9df8528e141e390553d5cbb
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-21 03:21:51 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -20,7 +20,8 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 listint_t *swap_node(listint_t *node, listint_t **list);
-
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
