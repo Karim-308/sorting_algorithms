d14f849508a559ca0cdcc2704d7b4c99f43034e3
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-21 02:40:32 +0200

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
  *                        using the Insertion sort algorithm.
  *
  * @list: A pointer to a doubly linked list
