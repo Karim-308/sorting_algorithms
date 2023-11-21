212a6b6e4c7f1aeb028d03bcabb6ee73b413255c
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-21 03:13:12 +0200

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,8 +12,10 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min_index;
 	int temp;
 
-	if (array == NULL) return;
-	if (size < 2) return;
+	if (array == NULL)
+		return;
+	if (size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
