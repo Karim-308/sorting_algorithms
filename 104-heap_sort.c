0c7cee997ed1d13529206520f83f8215225bf52d
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-22 03:34:46 +0200

    Update 104-heap_sort.c

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -9,9 +9,9 @@
  */
 void swap(int *array, int a, int b)
 {
-    int temp = array[a];
-    array[a] = array[b];
-    array[b] = temp;
+	int temp = array[a];
+	array[a] = array[b];
+	array[b] = temp;
 }
 
 /**
@@ -23,22 +23,22 @@ void swap(int *array, int a, int b)
  */
 void sift_down(int *array, size_t size, size_t root, size_t end)
 {
-    size_t max = root;
-    size_t left = 2 * root + 1;
-    size_t right = 2 * root + 2;
+	size_t max = root;
+	size_t left = 2 * root + 1;
+	size_t right = 2 * root + 2;
 
-    if (left < end && array[left] > array[max])
-        max = left;
+	if (left < end && array[left] > array[max])
+		max = left;
 
-    if (right < end && array[right] > array[max])
-        max = right;
+	if (right < end && array[right] > array[max])
+		max = right;
 
-    if (max != root)
-    {
-        swap(array, root, max);
-        print_array(array, size);
-        sift_down(array, size, max, end);
-    }
+	if (max != root)
+	{
+		swap(array, root, max);
+		print_array(array, size);
+		sift_down(array, size, max, end);
+	}
 }
 
 /**
@@ -49,20 +49,20 @@ void sift_down(int *array, size_t size, size_t root, size_t end)
  */
 void heap_sort(int *array, size_t size)
 {
-    int i;
-    if (array == NULL || size <= 1)
-    return;
+	if (array == NULL || size <= 1)
+		return;
 
+	int i;
 
-    /* Build the heap */
-    for (i = size / 2 - 1; i >= 0; i--)
-        sift_down(array, size, i, size);
-    
-    /* Extract elements from the heap */
-    for (i = size - 1; i > 0; i--)
-    {
-        swap(array, 0, i);
-        print_array(array, size);
-        sift_down(array, size, 0, i);
-    }
+	/* Build the heap */
+	for (i = size / 2 - 1; i >= 0; i--)
+		sift_down(array, size, i, size);
+
+	/* Extract elements from the heap */
+	for (i = size - 1; i > 0; i--)
+	{
+		swap(array, 0, i);
+		print_array(array, size);
+		sift_down(array, size, 0, i);
+	}
 }
