3dff0718f6f204243c6aeeeb8c7b98fe08fc9ba7
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-11-22 03:46:34 +0200

    Create 107-quick_sort_hoare.c

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,74 @@
+#include "sort.h"
+
+/**
+ * quick_sort_hoare - Sorts an array of integers in ascending order
+ *                    using the Quick sort algorithm (Hoare partition scheme).
+ *
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+
+    quick_sort_hoare_recursive(array, 0, size - 1, size);
+}
+
+/**
+ * quick_sort_hoare_recursive - Recursive helper function for quick_sort_hoare
+ *
+ * @array: The array to be sorted
+ * @low: The low index of the partition
+ * @high: The high index of the partition
+ * @size: The size of the array
+ */
+void quick_sort_hoare_recursive(int *array, int low, int high, size_t size)
+{
+    if (low < high)
+    {
+        int pivot = hoare_partition(array, low, high, size);
+
+        /* Recursively sort the partitions */
+        quick_sort_hoare_recursive(array, low, pivot, size);
+        quick_sort_hoare_recursive(array, pivot + 1, high, size);
+    }
+}
+
+/**
+ * hoare_partition - Hoare partition scheme for Quick Sort
+ *
+ * @array: The array to be sorted
+ * @low: The low index of the partition
+ * @high: The high index of the partition
+ * @size: The size of the array
+ *
+ * Return: The pivot index
+ */
+int hoare_partition(int *array, int low, int high, size_t size)
+{
+    int pivot = array[low];
+    int i = low - 1, j = high + 1, temp;
+
+    while (1)
+    {
+        do
+        {
+            i++;
+        } while (array[i] < pivot);
+
+        do
+        {
+            j--;
+        } while (array[j] > pivot);
+
+        if (i >= j)
+            return j;
+
+        /* Swap array[i] and array[j] */
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        print_array(array, size);
+    }
+}
